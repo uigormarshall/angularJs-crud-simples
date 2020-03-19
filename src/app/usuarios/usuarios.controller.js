74cdb1aaa9e43c0709f7107f0b31094df6bb646d
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-19 11:08:45 -0300

    Adiciona itens a tabela de usuarios

--- src/app/usuarios/usuarios.controller.js
+++ src/app/usuarios/usuarios.controller.js
@@ -3,5 +3,7 @@ angular.module('angularCrudSimples').controller('usuariosController', usuariosCo
 usuariosController.$inject = ['$scope', 'usuariosService'];
 
 function usuariosController($scope, usuariosService) {
-    $scope.screenName = usuariosService.getTitulo()
+    $scope.screenName = usuariosService.getTitulo();
+    $scope.tabelaColunas = ['id', 'nome', 'email', 'status'];
+    $scope.usuarios = usuariosService.getUsuarios();
 }
--- src/app/usuarios/usuarios.service.js
+++ src/app/usuarios/usuarios.service.js
@@ -4,5 +4,27 @@ function usuariosService() {
     function getTitulo() {
         return "Usuários ";
     }
-    return {getTitulo}
+    function getUsuarios() {
+        return [
+            {
+                "id":1,
+                "nome":"Uigor Marshall",
+                "email":"uigor.silva@acad.ifma.edu.br",
+                "status":"Ativo"
+            },
+            {
+                "id":2,
+                "nome":"Ana Maria",
+                "email":"ana@acad.ifma.edu.br",
+                "status":"Inativo"
+            },
+            {
+                "id":3,
+                "nome":"Marcio Braha Maria",
+                "email":"marcio@acad.ifma.edu.br",
+                "status":"Ativo"
+            }
+        ]
+    }
+    return {getTitulo, getUsuarios}
 }
